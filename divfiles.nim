a9cf9e7c603374858b3b6d2ee26c9dd33dbd5be1
Isaac Duan <ouseiryu@live.cn>
2019-08-02 01:04:24 +0800

    Update divfiles.nim

--- divfiles.nim
+++ divfiles.nim
@@ -1,20 +1,42 @@
-import streams
+import streams, os, strutils
 
-var strm = newFileStream("./aaa.wmv",fmRead)
-var buff: array[1024,char]
+echo "param count is: ", paramCount()
+var rfilename = ""
+var divnum:uint = 0
+try:
+  rfilename = paramStr(1)
+  divnum = paramStr(2).parseUInt()
+  echo "file name: ", rfilename, "  ", "Div parts: ", divnum
+except:
+  echo "Error: illegal params"
+  quit(0)
 
-strm.read(buff)
-echo buff
-echo strm.getPosition()
-while true:
-  for x in 0..1023:
-    buff[x] = '\x00'
-  try:
-    strm.read(buff)
-    echo strm.getPosition()
-    echo buff.len()
-  except:
+var f:File
+try:
+  f = open(rfilename)
+except:
+  echo "Error: file not found"
+  quit(0)
+
+var filesize = f.getFileSize()
+if filesize <= 0:
+  raise newException(ValueError, "File size is 0")
+  
+var wbuff:seq[byte] = @[]
+var blocksize = uint64(filesize /% divnum.int64) + 1
+#需要使用setLen手动增长填充seq，默认填充0
+wbuff.setLen(blocksize)
+echo blocksize
+
+for x in 0..divnum:
+  #File的read_bytes方法不会自动增长seq，可以使用setLen方法扩充seq(默认填充0)
+  var rlen = f.readBytes(wbuff, 0, blocksize)
+  echo "读取数据量", rlen
+  if rlen == 0:
     break
-echo buff
-echo buff.len()
-strm.close()
+  var fw = open(rfilename & int(x).intToStr(), fmWrite)
+  echo "正在写入", rfilename & int(x).intToStr()
+  discard fw.writeBytes(wbuff, 0, rlen)
+  fw.close()
+  
+f.close()  
